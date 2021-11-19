6f49245b9b4dc75ba59b812204322967e913ef45
Asahi Iwase <asahi.iwase.engineer@gmail.com>
2021-11-19 21:40:24 +0900

    Create folder model

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,2 +1,4 @@
 class Folder < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  validates :sticky, presence: true
 end
--- spec/factories/folders.rb
+++ spec/factories/folders.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :folder do
-    
+    title { Faker::Lorem.word }
+    sticky { false }
   end
 end
