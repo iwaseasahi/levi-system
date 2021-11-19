b84b80faf889eebd21103cba57a3d3fb0a105241
Asahi Iwase <asahi.iwase.engineer@gmail.com>
2021-11-19 12:21:17 +0900

    御言葉の箇所は訳で一意であることをテストする

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :book do
-    
+    version { rand(1..2) }
+    book_name
+    chapter { Faker::Number.within(range: 1..150) }
+    verse { Faker::Number.within(range: 0..176) }
+    word { Faker::Lorem.sentence }
   end
 end
