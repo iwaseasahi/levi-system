b34931cbb80b8476836c5f0b0a1128364b830845
Asahi Iwase <asahi.iwase.engineer@gmail.com>
2021-11-19 11:32:18 +0900

    Update association & validates for book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+  belongs_to :book_name, dependent: :destroy
+
+  validates :version, presence: true
+  validates :chapter, presence: true
+  validates :verse, presence: true
 end
