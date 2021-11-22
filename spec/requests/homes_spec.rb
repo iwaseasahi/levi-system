97acc29c9fe40a2dab9cb34d276f044c9548c148
Asahi Iwase <asahi.iwase.engineer@gmail.com>
2021-11-22 09:44:22 +0900

    Add spec for homes request

--- spec/requests/homes_spec.rb
+++ spec/requests/homes_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'リクエストが成功すること' do
+      get root_path
+
+      expect(response).to have_http_status 200
+    end
   end
 end
